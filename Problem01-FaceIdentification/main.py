3c5995ba3d4317c75e434ef04d0ee28b24d1c104
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-08-15 18:53:27 -0300

    adding code to show the first eigenface to mais.py, but the eigenfaces values should be floats

--- Problem01-FaceIdentification/main.py
+++ Problem01-FaceIdentification/main.py
@@ -1,6 +1,9 @@
 from faceid_database import YaleFaces
 from faceid_database import ORL
 from eigenface import Eigenface
+import matplotlib.pyplot as plt
 
 yale = YaleFaces('./databases/yalefaces/')
-Eigenface.calculate(yale.images)
+eigenfaces = Eigenface.calculate(yale.images)
+plt.imshow(eigenfaces[0], cmap='Greys_r')
+plt.show()
