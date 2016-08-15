af1b2c4d9d1b8e1197a7c0cffa3a657acb5eeec4
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-08-15 18:40:26 -0300

    creating eigenfaces, just need to reshape itit

--- Problem01-FaceIdentification/eigenface.py
+++ Problem01-FaceIdentification/eigenface.py
@@ -19,17 +19,29 @@ class Eigenface:
 		images = []
 		for image in array_of_images:
 			images.append(image - mean_face)
-		return np.array(images)	
+		return np.array(images)
+
+	@staticmethod
+	def order_eigenvectors_by_eigenvalues(eigenvectors, eigenvalues):
+		idx = eigenvalues.argsort()[::-1]   
+		eigenvalues = eigenvalues[idx]
+		return eigenvectors[:,idx]
 
 	@staticmethod
 	def calculate(images, number_of_eigenfaces = 5):
 		images = np.array(images)
+		print images.shape
 		mean_face = Eigenface.calculate_mean_face(images)
 		immf = Eigenface.images_minus_mean_face(images, mean_face)
-		images = Eigenface.transform_images_to_array(immf)
-		covariance_matrix = np.cov(images)
+		images = Eigenface.transform_images_to_array(immf) # num_img X (number of pixeis in images)
+		covariance_matrix = np.cov(images) # num_img x num_img
 		eigenvalues, eigenvectors = LA.eig(covariance_matrix)
+		eigenvectors = Eigenface.order_eigenvectors_by_eigenvalues(eigenvectors, eigenvalues)
 		eigenvectors = eigenvectors[:number_of_eigenfaces]
-		print eigenvectors
-		
-		
+		transposed_images = images.transpose()
+		eigenfaces = []
+		for eigenvector in eigenvectors:
+			multiplication = np.dot(transposed_images, eigenvector)
+			eigenfaces.append(multiplication)
+		eigenfaces = np.array(eigenfaces)
+		print eigenfaces.shape
