ab3ce7954a1f2110c21e21996611c56b45a23929
MatheusAN <matheus.agio@hotmail.com>
2016-08-18 14:36:21 -0300

    apparently find eigenfaces is correct, but the recostruction is not ok

--- Problem01-FaceIdentification/eigenface.py
+++ Problem01-FaceIdentification/eigenface.py
@@ -4,77 +4,89 @@ from numpy import linalg as LA
 
 class Eigenface:
 
-    def __init__(self,images):
-        self.images = np.array(images)
-        self.eigenfaces = None
+    # Images: Array of images that will be the database
+    def __init__(self, images):
+        im = np.array(images)
+        n_images, height, width = im.shape
+        self.n_images = n_images
+        self.image_height = height
+        self.image_width = width
+        self.images = self.transform_images_to_array(im)
         self.projected_images = None
         self.mean_face = None
+        self.eigenfaces = None
+
+    def get_image(self, index):
+        return self.images[index].reshape(self.image_height, self.image_width)
+
+    def reconstruct_image(self, index):
+        projected_image = self.projected_images[index]
+        eigenfaces = self.eigenfaces
+        multiplication = np.dot(eigenfaces.transpose(), projected_image)
+        result = np.add(multiplication, self.mean_face).reshape(
+            self.image_height, self.image_width)
+        return result
 
     def calculate_mean_face(self, array_of_images):
-        mean = np.mean(array_of_images, axis=0, dtype=np.float32)
-        self.mean_face = np.array(mean)
-        return np.array(mean)
+        return np.average(array_of_images, axis=0)
 
     def transform_images_to_array(self, images_in_matrix_form):
-        images = []
-        for image in images_in_matrix_form:
-            images.append(image.flatten())
-        return np.array(images)
+        length, height, width = images_in_matrix_form.shape
+        return images_in_matrix_form.reshape(length, height*width)
 
-    def images_minus_mean_face(self, array_of_images, mean_face):
-        images = []
-        for image in array_of_images:
-            images.append(image - mean_face)
-        return np.array(images)
+    def images_minus_mean_face(self, array_of_images):
+        return array_of_images - self.get_mean_face()
 
     def order_eigenvectors_by_eigenvalues(self, eigenvectors, eigenvalues):
         idx = eigenvalues.argsort()[::-1]
-        return eigenvectors[:, idx]
+        return eigenvectors[idx]
 
-    def find_eigenfaces(self, number_of_eigenfaces=5):
-        images = self.images # num_imgs X heigth X width
-        n_of_images, heigth, width = images.shape
+    def find_eigenfaces(self, array_of_images, number_of_eigenfaces=5):
 
-        mean_face = self.calculate_mean_face(images)  # heigth X width
-        immf = self.images_minus_mean_face(
-            images, mean_face)  # heigth X width
+        # num_imgs X (number of pixels in images)
+        images = self.images_minus_mean_face(array_of_images)
 
-        images = self.transform_images_to_array(
-            immf)  # num_img X (number of pixels in images)
-
-        covariance_matrix = np.cov(images)  # num_img x num_img
+        # num_img x num_img
+        covariance_matrix = np.dot(images, images.T)
         eigenvalues, eigenvectors = LA.eig(covariance_matrix)
+        eigenvectors = eigenvectors.transpose()
         eigenvectors = self.order_eigenvectors_by_eigenvalues(
             eigenvectors, eigenvalues)
-        eigenvectors = eigenvectors[:number_of_eigenfaces]  # num_img X 1
+        # Each eigenvector has num_img X 1
+        eigenvectors = eigenvectors[:number_of_eigenfaces]
         eigenvectors = np.real(eigenvectors)
 
-        transposed_images = images.transpose() #  (number of pixels in images) X num_img
+        # (number of pixels in images) X num_img
+        transposed_images = images.transpose()
         eigenfaces = []
         for eigenvector in eigenvectors:
-            multiplication = np.dot(transposed_images, eigenvector)
-            eigenfaces.append(multiplication.reshape(heigth, width))
-        self.eigenfaces = np.array(eigenfaces)
-        return self.eigenfaces
-
-    def get_eigenfaces(self):
-        if self.eigenfaces == None:
-            self.find_eigenfaces()
-        return self.eigenfaces
+            # result is an array with (number of pixels in images) size
+            result = np.dot(transposed_images, eigenvector)
+            eigenfaces.append(result/LA.norm(result))
+        eigenfaces = np.array(eigenfaces)
+        # n_eigenfaces X (number of pixels in images)
+        self.eigenfaces = eigenfaces
+        return eigenfaces
 
     def get_mean_face(self):
-        if self.mean_face == None:
-            self.calculate_mean_face(self.images)
+        if self.mean_face is None:
+            self.mean_face = self.calculate_mean_face(self.images)
         return self.mean_face
 
     def project_image(self, image, eigenfaces, mean_face):
-        return np.dot(eigenfaces, (image - mean_face))
+        # (number of pixels in images) X number_of_eigenfaces *
+        # (number of pixels in images) X 1
+        return np.dot(eigenfaces, (image - mean_face).transpose())
 
-    def train(self):
-        eigenfaces = self.transform_images_to_array(self.get_eigenfaces())
-        images = self.transform_images_to_array(self.images)
-        mean_face = self.get_mean_face().flatten()
-        self.projected_images = []
+    def project_images(self, images, eigenfaces, mean_face):
+        tmp = []
         for image in images:
-            self.projected_images.append(self.project_image(image, eigenfaces, mean_face))
-        return self.projected_images
+            tmp.append(
+                self.project_image(image, eigenfaces, mean_face))
+        self.projected_images = np.array(tmp)
+
+    def train(self, number_of_eigenfaces=5):
+        eigenfaces = self.find_eigenfaces(self.images, number_of_eigenfaces)
+        images = self.images
+        mean_face = self.get_mean_face()
+        self.project_images(images, eigenfaces, mean_face)
--- Problem01-FaceIdentification/main.py
+++ Problem01-FaceIdentification/main.py
@@ -2,9 +2,14 @@ from faceid_database import YaleFaces
 from faceid_database import ORL
 from eigenface import Eigenface
 import matplotlib.pyplot as plt
+import numpy as np
 
 yale = YaleFaces('./databases/yalefaces/')
 recognizer = Eigenface(yale.images)
-images = recognizer.train()
-for image in images:
-	print image
+recognizer.train(5)
+image = recognizer.get_image(10)
+plt.imshow(image)
+plt.show()
+reconstructed_img = recognizer.reconstruct_image(10)
+plt.imshow(reconstructed_img)
+plt.show()
