31137b90819ab13d8611cb596365f54f471cf7ba
MatheusAN <matheus.agio@hotmail.com>
2016-08-19 15:24:39 -0300

    adding basic cross validation code

--- Problem01-FaceIdentification/main.py
+++ Problem01-FaceIdentification/main.py
@@ -2,11 +2,27 @@ from faceid_database import YaleFaces
 from faceid_database import ORL
 from eigenface import Eigenface
 import matplotlib.pyplot as plt
-import numpy as np
 
 yale = YaleFaces('./databases/yalefaces/')
-recognizer = Eigenface(yale.images)
-recognizer.train(5)
-reconstructed_img = recognizer.reconstruct_image(10)
-plt.imshow(reconstructed_img)
-plt.show()
+for label in yale.class_labels:
+    print 'Testing remotion of label: ', label
+    images = []
+    subjects = []
+    test_images = []
+    test_subjects = []
+    for index, val in enumerate(yale.classes):
+        if val == label:
+            test_subjects.append(yale.subjects[index])
+            test_images.append(yale.images[index])
+        else:
+            subjects.append(yale.subjects[index])
+            images.append(yale.images[index])
+    print subjects
+    recognizer = Eigenface(images, subjects)
+    recognizer.train(100)
+    correct = 0.0
+    for index, subject in enumerate(test_subjects):
+        if subject == recognizer.recognize(test_images[index]):
+            correct += 1
+    accuracy = correct/len(test_subjects)
+    print 'Accuracy: ', accuracy * 100, '%'
