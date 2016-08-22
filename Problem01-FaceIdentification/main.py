83b73fa1615ce10b8910eefd23aadd87b0fd6697
MatheusAN <matheus.agio@hotmail.com>
2016-08-21 21:41:22 -0300

    adding percentage of orl tsts concluded, adding multiple orl tests

--- Problem01-FaceIdentification/main.py
+++ Problem01-FaceIdentification/main.py
@@ -4,13 +4,17 @@ from eigenface import Eigenface
 import numpy as np
 import random
 import platform
+from sys import stdout
 
+
+CLEAR_LINE = '\x1b[2K'
 
 def print_system_info():
     print '-----------------------SYSTEM INFORMATION--------------------------'
     print '-> System: ', platform.system(), platform.release()
     print '-> Python Version: ', platform.python_version()
     print '-------------------------------------------------------------------'
+
 
 def calculate_yale():
     print 'Testing YaleFaces database'
@@ -41,45 +45,49 @@ def calculate_yale():
 
 def calculate_orl():
     print 'Testing ORL database\n'
+    number_of_eigenfaces_on_each_test = [5, 10, 50, 100, 200, 300]
     orl = ORL('./databases/orl_faces/')
     n_of_images = len(orl.images)
     k_fold = 10
     n_of_itens_per_fold = n_of_images / k_fold
-    means = []
-    for k in xrange(k_fold):
-        images = []
-        subjects = []
-        test_images = []
-        test_subjects = []
-        n_of_correct_guesses = 0
-        n_of_tries = 0
-        print 'Generating', k + 1, 'test sample'
-        indexes_to_test = random.sample(
-            range(n_of_images), n_of_itens_per_fold)
-        for index in xrange(n_of_images):
-            if index in indexes_to_test:
-                test_images.append(orl.images[index])
-                test_subjects.append(orl.subjects[index])
-            else:
-                images.append(orl.images[index])
-                subjects.append(orl.subjects[index])
+    for n_of_eigenfaces in number_of_eigenfaces_on_each_test:
+        print 'Testing with', n_of_eigenfaces, 'eigenfaces'
+        means = []
+        for k in xrange(k_fold):
+            stdout.write('\r%d%%' % (k*10))
+            stdout.flush()
+            images = []
+            subjects = []
+            test_images = []
+            test_subjects = []
+            n_of_correct_guesses = 0
+            n_of_tries = 0
+            indexes_to_test = random.sample(
+                range(n_of_images), n_of_itens_per_fold)
+            for index in xrange(n_of_images):
+                if index in indexes_to_test:
+                    test_images.append(orl.images[index])
+                    test_subjects.append(orl.subjects[index])
+                else:
+                    images.append(orl.images[index])
+                    subjects.append(orl.subjects[index])
 
-        print 'Training recognizer'
-        recognizer = Eigenface(images, subjects)
-        recognizer.train(10)
+            recognizer = Eigenface(images, subjects)
+            recognizer.train(n_of_eigenfaces)
 
-        print 'Calculating tests\n'
-        for index, test_subject in enumerate(test_subjects):
-            n_of_tries += 1
-            result = recognizer.recognize(test_images[index])
-            if test_subject == result:
-                n_of_correct_guesses += 1
-        means.append(n_of_correct_guesses / float(n_of_tries))
-    print 'Accuracy:', np.mean(means)*100, '%'
-    print 'Standart Deviation:', np.std(means)
+            for index, test_subject in enumerate(test_subjects):
+                n_of_tries += 1
+                result = recognizer.recognize(test_images[index])
+                if test_subject == result:
+                    n_of_correct_guesses += 1
+            means.append(n_of_correct_guesses / float(n_of_tries))
+        stdout.write('\r' + CLEAR_LINE)
+        print 'Accuracy:', np.mean(means)*100, '%'
+        print 'Standart Deviation:', np.std(means), '\n'
+
 
 # MAIN
 print_system_info()
-calculate_yale()
-print '----------------------------------------------------------------------'
+# calculate_yale()
+print '-------------------------------------------------------------------'
 calculate_orl()
