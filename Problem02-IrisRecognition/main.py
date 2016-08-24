4d2fb59b6f732a47f599b2e95665d4bfb69d35f4
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-08-24 18:49:10 -0300

    initial thoughts

---
+++ Problem02-IrisRecognition/main.py
@@ -0,0 +1,20 @@
+# Filtro de canny (python canny)
+# ler todas as images da base
+# preprocessar as imagens
+# aplicar o filtro de canny em cada uma delas
+# aplicar a transformada circular hough (tentar implementar ela)
+# com isso eu detectei a pupila
+# dado o centro da pupila conseguimos detectar a iris
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('/nobackup/bcc/man13/nobackup/CASIA-Iris-Lamp-100/001/R/S2001R01.jpg',0)
+edges = cv2.Canny(img,200,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
