581276647270bd33244e3f9a07f96af05c7eb630
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-08-29 17:56:06 -0300

    thread that probably will help

--- Problem02-IrisRecognition/main.py
+++ Problem02-IrisRecognition/main.py
@@ -5,11 +5,12 @@
 # aplicar a transformada circular hough (tentar implementar ela)
 # com isso eu detectei a pupila
 # dado o centro da pupila conseguimos detectar a iris
+# http://stackoverflow.com/questions/9860667/writing-robust-color-and-size-invariant-circle-detection-with-opencv-based-on
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('/nobackup/bcc/man13/nobackup/CASIA-Iris-Lamp-100/001/R/S2001R01.jpg',0)
+img = cv2.imread('/nobackup/bcc/man13/nobackup/CASIA-Iris-Lamp-100/001/R/S2001R01.jpg')
 edges = cv2.Canny(img,200,200)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
