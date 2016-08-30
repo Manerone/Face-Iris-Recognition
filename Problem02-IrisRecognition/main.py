80efcf9fd9e1ac084e4f139908f6c17caeea0951
MatheusAN <matheus.agio@hotmail.com>
2016-08-30 14:10:42 -0300

    making progess on the pupil detection

--- Problem02-IrisRecognition/main.py
+++ Problem02-IrisRecognition/main.py
@@ -8,15 +8,49 @@
 # http://stackoverflow.com/questions/9860667/writing-robust-color-and-size-invariant-circle-detection-with-opencv-based-on
 # http://stackoverflow.com/questions/10716464/what-are-the-correct-usage-parameter-values-for-houghcircles-in-opencv-for-iris
 import cv2
+from casia_iris_image_loader import ImageLoaderCASIAIris
 import numpy as np
+# from PIL import Image
 from matplotlib import pyplot as plt
 
-img = cv2.imread('/nobackup/bcc/man13/nobackup/CASIA-Iris-Lamp-100/001/R/S2001R01.jpg')
-edges = cv2.Canny(img,200,200)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+def show_img(img):
+    plt.imshow(img, cmap='gray')
+    plt.show()
 
-plt.show()
+
+def find_circles(image):
+    accumulator_threshold = 70
+    circles = None
+    while circles is None:
+        circles = cv2.HoughCircles(image, cv2.cv.CV_HOUGH_GRADIENT,
+                                   1, 1, param2=accumulator_threshold)
+        accumulator_threshold = accumulator_threshold - 1
+    circles = np.uint16(np.around(circles))
+    return circles[0][0]
+
+
+def pre_process_img(image):
+    img = image
+    img_with_blur = cv2.medianBlur(img, 15)
+    edges = cv2.Canny(img_with_blur, 50, 200)
+    # show_img(edges)
+    return edges
+
+
+casia = ImageLoaderCASIAIris('./databases/CASIA-Iris-Lamp-100')
+for index, image in enumerate(casia.images):
+    print casia.subjects[index]
+    processed_img = pre_process_img(image)
+    circles = find_circles(processed_img)
+    cv2.circle(image, (circles[0], circles[1]), circles[2], (0, 255, 0), 1)
+    cv2.circle(image, (circles[0], circles[1]), 2, (0, 0, 255), 3)
+    show_img(image)
+# img = Image.open(
+#     './databases/CASIA-Iris-Lamp-100/085/R/S2085R02.jpg').convert('L')
+# img = np.array(img, 'uint8')
+# processed_img = pre_process_img(img)
+# circles = find_circles(processed_img)
+# cv2.circle(img, (circles[0], circles[1]), circles[2], (0, 255, 0), 1)
+# cv2.circle(img, (circles[0], circles[1]), 2, (0, 0, 255), 3)
+# show_img(img)
