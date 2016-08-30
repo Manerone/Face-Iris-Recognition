f5073caa385e5906f773d74c6daba3e906fd21ef
MatheusAN <matheus.agio@hotmail.com>
2016-08-30 11:35:55 -0300

    image loader for casia

---
+++ Problem02-IrisRecognition/casia_iris_image_loader.py
@@ -0,0 +1,41 @@
+import os
+from PIL import Image
+import numpy as np
+
+
+class ImageLoaderCASIAIris(object):
+    """docstring for ImageLoaderCASIAIris"""
+
+    def __init__(self, path):
+        self.path = os.path.abspath(path)
+        self.get_images_and_subjects(self.path)
+
+    def get_images_and_subjects(self, path):
+        self.images = []
+        self.subjects = []
+        for subject_dir in self.get_directories_from(path):
+            _, subject = os.path.split(subject_dir)
+            subject = int(subject)
+            eye_dirs = self.get_directories_from(subject_dir)
+            for left_and_right_eye_dir in eye_dirs:
+                for image in self.get_files_from(left_and_right_eye_dir):
+                    image_pil = Image.open(image).convert('L')
+                    image_np = np.array(image_pil, 'uint8')
+                    self.images.append(image_np)
+                    self.subjects.append(subject)
+
+    def get_directories_from(self, path):
+        directories = []
+        for element in os.listdir(path):
+            path_to_element = os.path.join(path, element)
+            if os.path.isdir(path_to_element):
+                directories.append(path_to_element)
+        return directories
+
+    def get_files_from(self, path):
+        files = []
+        for element in os.listdir(path):
+            path_to_element = os.path.join(path, element)
+            if os.path.isfile(path_to_element):
+                files.append(path_to_element)
+        return files
