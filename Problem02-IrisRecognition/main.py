9eafe86c4b9d61370f4b53c152fca8d31b04690d
MatheusAN <matheus.agio@hotmail.com>
2016-09-01 11:59:38 -0300

    improving iris detections

--- Problem02-IrisRecognition/main.py
+++ Problem02-IrisRecognition/main.py
@@ -12,7 +12,6 @@ from casia_iris_image_loader import ImageLoaderCASIAIris
 import numpy as np
 from PIL import Image
 from operator import itemgetter
-from matplotlib import pyplot as plt
 
 
 def show_img(imgs):
@@ -33,39 +32,30 @@ def find_pupil(image):
 
 def get_points_near_circle_perimeter(x, y, r, img, num_of_points=360):
     points = []
-    distance_of_points = (2 * np.pi)/num_of_points
+    distance_of_points = (2 * np.pi) / num_of_points
     for i in range(num_of_points):
-        teta = i*distance_of_points
-        x_p = x + int(r*np.cos(teta))
-        y_p = y + int(r*np.sin(teta))
-        points.append(img[x_p][y_p])
+        teta = i * distance_of_points
+        y_p = y + int(r * np.cos(teta))
+        x_p = x + int(r * np.sin(teta))
+        points.append(img[y_p][x_p])
     return np.array(points)
 
 
 def find_iris(x_pupil, y_pupil, r_pupil, img, value=4):
-    # pega X pontos em cada circulo em uma direcao e calcula a diferenca dele para o do circulo anterior
-    # soma essas diferencas
-    max_iterations = 15
-    iterations = 1
-    image = img.copy()
+    image = cv2.equalizeHist(img)
+    n_of_rows, n_of_columns = img.shape
+    max_r = min((n_of_rows - y_pupil, n_of_columns - x_pupil))
     x, y, r_b = x_pupil, y_pupil, r_pupil
-    r_a = r_b + 5
+    r_a = r_b + 20
     variations = []
-    while iterations < max_iterations:
-        # cv2.circle(image, (x, y), r_a, (0, 255, 0), 1)
-        # cv2.circle(image, (x, y), 2, (0, 0, 255), 3)
-        # show_img([image])
-        points_before = get_points_near_circle_perimeter(x, y, r_b, img)
-        points_after = get_points_near_circle_perimeter(x, y, r_a, img)
-        # print abs(mean_after - mean_before)
-        # if abs(mean_after - mean_before) > value:
-        #     break
-        # else:
+    while r_a < max_r:
+        points_before = get_points_near_circle_perimeter(x, y, r_b, image)
+        points_after = get_points_near_circle_perimeter(x, y, r_a, image)
         variations.append((r_a, sum(abs(points_after - points_before))))
         r_b = r_a
         r_a += 5
-        iterations +=1
-    return x, y, max(variations,key=itemgetter(1))[0]
+    return x, y, max(variations, key=itemgetter(1))[0]
+
 
 def pre_process_img(image):
     # Threshold.
@@ -85,7 +75,7 @@ def pre_process_img(image):
     mask = np.zeros((h + 2, w + 2), np.uint8)
 
     # Floodfill from point (0, 0)
-    cv2.floodFill(im_floodfill, mask, (w/2, h - 1), 0)
+    cv2.floodFill(im_floodfill, mask, (w / 2, h - 1), 0)
 
     # Invert floodfilled image
     im_floodfill_inv = cv2.bitwise_not(im_floodfill)
@@ -104,24 +94,29 @@ def pre_process_img(image):
 
 casia = ImageLoaderCASIAIris('./databases/CASIA-Iris-Lamp-100')
 for index, image in enumerate(casia.images):
-    print casia.subjects[index]
     processed_img = pre_process_img(image)
     x_pupil, y_pupil, r_pupil = find_pupil(processed_img)
-    cv2.circle(image, (x_pupil, y_pupil), r_pupil, (255, 255, 255), 1)
-    cv2.circle(image, (x_pupil, y_pupil), 2, (0, 0, 255), 3)
-    image = cv2.equalizeHist(image)
     x_iris, y_iris, r_iris = find_iris(x_pupil, y_pupil, r_pupil, image)
+
+    cv2.circle(image, (x_pupil, y_pupil), r_pupil, (0, 255, 0), 1)
+    cv2.circle(image, (x_pupil, y_pupil), 2, (0, 0, 255), 3)
+
     cv2.circle(image, (x_iris, y_iris), r_iris, (255, 255, 0), 1)
     cv2.circle(image, (x_iris, y_iris), 2, (0, 0, 255), 3)
+
     show_img([processed_img, image])
 # image = Image.open(
 #     './databases/CASIA-Iris-Lamp-100/097/L/S2097L03.jpg').convert('L')
 # image = np.array(image, 'uint8')
 # processed_img = pre_process_img(image)
 # x_pupil, y_pupil, r_pupil = find_pupil(processed_img)
-# cv2.circle(image, (x_pupil, y_pupil), r_pupil, (0, 255, 0), 1)
-# cv2.circle(image, (x_pupil, y_pupil), 2, (0, 0, 255), 3)
+#
 # x_iris, y_iris, r_iris = find_iris(x_pupil, y_pupil, r_pupil, image)
+#
+# cv2.circle(image, (x_pupil, y_pupil), r_pupil, (255, 255, 255), 1)
+# cv2.circle(image, (x_pupil, y_pupil), 2, (255, 255, 255), 3)
+#
 # cv2.circle(image, (x_iris, y_iris), r_iris, (0, 255, 0), 1)
 # cv2.circle(image, (x_iris, y_iris), 2, (0, 0, 255), 3)
+#
 # show_img([processed_img, image])
