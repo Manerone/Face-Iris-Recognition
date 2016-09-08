fbbb287bfa8635b7b540a6ae974138152032d81a
MatheusAN <matheus.agio@hotmail.com>
2016-09-08 14:05:09 -0300

    changing methods that were in main to a class

---
+++ Problem02-IrisRecognition/iris_recognizer.py
@@ -0,0 +1,141 @@
+import cv2
+import numpy as np
+from operator import itemgetter
+import pywt
+import scipy.spatial
+
+
+class IrisRecognizer:
+    """docstring for IrisRecognizer"""
+
+    def __init__(self, subjects, images, acceptance_threshold=0.7):
+        self.subjects = subjects
+        self.images = images
+        self.signatures = []
+
+    def train(self):
+        for image in self.images:
+            self.process_image(image)
+
+    def verify(self, subject, image):
+        img_signature = self.process_image(image)
+        distance = scipy.spatial.distance.hamming(img_signature,)
+
+    def process_image(self, image):
+        processed_img = self.pre_process_img(image)
+        x_pupil, y_pupil, r_pupil = self.find_pupil(processed_img)
+        pupil_coords = (x_pupil, y_pupil, r_pupil)
+        x_iris, y_iris, r_iris = self.find_iris(pupil_coords, image)
+        iris_coords = (x_iris, y_iris, r_iris)
+        normalized_iris = self.normalize_iris(
+            pupil_coords, iris_coords, image)
+        signature = self.get_image_signature(normalized_iris)
+        self.signatures.append(signature)
+
+    def find_pupil(self, image):
+        accumulator_threshold = 150
+        circles = None
+        while circles is None:
+            circles = cv2.HoughCircles(image, cv2.cv.CV_HOUGH_GRADIENT,
+                                       1, 1, param2=accumulator_threshold)
+            accumulator_threshold = accumulator_threshold - 1
+        circles = np.uint16(np.around(circles[0, :]))
+        return circles[0]
+
+    def get_points_near_circle_perimeter(self, x, y, r, img, n_of_points=360):
+        points = []
+        distance_of_points = (2 * np.pi) / n_of_points
+        for i in range(n_of_points):
+            teta = i * distance_of_points
+            y_p = y + int(r * np.cos(teta))
+            x_p = x + int(r * np.sin(teta))
+            points.append(img[y_p][x_p])
+        return np.array(points)
+
+    def find_iris(self, (x_pupil, y_pupil, r_pupil), img, value=4):
+        image = cv2.equalizeHist(img)
+        n_of_rows, n_of_columns = img.shape
+        max_r = min((n_of_rows - y_pupil, n_of_columns - x_pupil))
+        x, y, r_b = x_pupil, y_pupil, r_pupil
+        r_a = r_b + 5
+        variations = []
+        points_before = self.get_points_near_circle_perimeter(x, y, r_b, image)
+        iterations = 0
+        max_iterations = 30
+        while r_a < max_r and iterations < max_iterations:
+            points_after = self.get_points_near_circle_perimeter(
+                x, y, r_a, image)
+            variations.append((r_a, sum(abs(points_after - points_before))))
+            points_before = points_after
+            r_a += 5
+            iterations += 1
+        return x, y, max(variations, key=itemgetter(1))[0]
+
+    def pre_process_img(self, image):
+        # Threshold.
+        # Set values equal to or above 35 to 255.
+        # Set values below 35 to 0.
+
+        img = image
+        img_with_blur = cv2.medianBlur(img, 25)
+        th, im_th = cv2.threshold(img_with_blur, 25, 255, cv2.THRESH_BINARY)
+
+        # Copy the thresholded image.
+        im_floodfill = im_th.copy()
+
+        # Mask used to flood filling.
+        # Notice the size needs to be 2 pixels than the image.
+        h, w = im_th.shape[:2]
+        mask = np.zeros((h + 2, w + 2), np.uint8)
+
+        # Floodfill from point (0, 0)
+        cv2.floodFill(im_floodfill, mask, (w / 2, h - 1), 0)
+
+        # Invert floodfilled image
+        im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+        # Combine the two images to get the foreground.
+        im_out = im_th & im_floodfill_inv
+        # cv2.imshow("Original Image", image)
+        # cv2.imshow("Thresholded Image", im_th)
+        # cv2.imshow("Floodfilled Image", im_floodfill)
+        # cv2.imshow("Inverted Floodfilled Image", im_floodfill_inv)
+        # cv2.imshow("Foreground", im_out)
+        # cv2.waitKey(0)
+        canny = cv2.Canny(im_out, 50, 200)
+        return canny
+
+    def normalize_iris(self, (x_pupil, y_pupil, r_pupil),
+                       (x_iris, y_iris, r_iris),
+                       image, n_of_divisions=360):
+        distance_of_each_division = (2 * np.pi) / n_of_divisions
+        normalized_image = []
+        for division in xrange(n_of_divisions):
+            teta = distance_of_each_division * division
+            points = []
+            for radius in xrange(int(r_pupil), int(r_iris + 1)):
+                y_p = y_iris + int(radius * np.cos(teta))
+                x_p = x_iris + int(radius * np.sin(teta))
+                points.append(image[y_p][x_p])
+            normalized_image.append(points)
+        normalized_image = np.array(normalized_image).T
+        resized_image = cv2.resize(normalized_image,
+                                   (256, 32), interpolation=cv2.INTER_CUBIC)
+        return resized_image
+
+    def binarize_signature(self, matrix):
+        signature = []
+        for array in matrix:
+            for element in array:
+                if element >= 0:
+                    signature.append(1)
+                else:
+                    signature.append(0)
+        return signature
+
+    def get_image_signature(self, image):
+        cA, _ = pywt.dwt2(image, 'haar')
+        cA, _ = pywt.dwt2(cA, 'haar')
+        cA, _ = pywt.dwt2(cA, 'haar')
+        cA, (cH, cV, cD) = pywt.dwt2(cA, 'haar')
+        return self.binarize_signature(cH + cV + cD)
