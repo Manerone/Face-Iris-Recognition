7c251ff095428601480668c430cd83260bf49a62
MatheusAN <matheus.agio@hotmail.com>
2016-09-09 13:52:09 -0300

    verification is now working

--- Problem02-IrisRecognition/main.py
+++ Problem02-IrisRecognition/main.py
@@ -4,9 +4,7 @@ import scipy.spatial.distance as distance
 import platform
 import numpy as np
 import pandas
-from sys import stdout
-
-CLEAR_LINE = '\x1b[2K'
+import matplotlib.pyplot as plt
 
 
 def print_system_info():
@@ -16,9 +14,17 @@ def print_system_info():
     print '==================================================================='
 
 
-def print_percent(current, total):
-    stdout.write('\r%d%%' % ((current/float(total))*100))
-    stdout.flush()
+def showMetrics(metrics):
+    x = []
+    y = []
+    for pt in metrics:
+        x.append(pt[1])
+        y.append(pt[2])
+    plt.axis([0, 100, 0, 100])
+    plt.ylabel('FAR')
+    plt.xlabel('FRR')
+    plt.plot(x, y)
+    plt.show()
 
 
 def calculate_distances(subjects, signatures):
@@ -34,18 +40,24 @@ def calculate_distances(subjects, signatures):
 def verify(subjects, distances, threshold):
     frr = 0
     far = 0
+    impostor_scores = 0
+    genuine_scores = 0
     n_of_signatures = len(subjects)
     for i in xrange(n_of_signatures):
         for j in xrange(n_of_signatures):
             if i != j:
                 dist = distances[i][j]
-                if subjects[i] == subjects[j] and dist > threshold:
-                    frr += 1
-                elif subjects[i] != subjects[j] and dist <= threshold:
-                    far += 1
+                if subjects[i] == subjects[j]:
+                    genuine_scores += 1
+                    if dist > threshold:
+                        frr += 1
+                elif subjects[i] != subjects[j]:
+                    impostor_scores += 1
+                    if dist <= threshold:
+                        far += 1
 
-    frr = (frr/float(n_of_signatures*n_of_signatures)*100)
-    far = (far/float(n_of_signatures*n_of_signatures)*100)
+    frr = (frr / float(genuine_scores) * 100)
+    far = (far / float(impostor_scores) * 100)
     return (frr, far)
 
 
@@ -66,6 +78,7 @@ def iris_verification(subjects, signatures):
         frr, far = verify(subjects, distances, threshold)
         measures.append([threshold, frr, far])
     print_far_frr_table(measures)
+    showMetrics(measures)
     minimun = abs(measures[0][1] - measures[0][2])
     eer = 0
     for index, measure in enumerate(measures):
@@ -80,7 +93,7 @@ def iris_identification(subjects, normalized_irises):
 
 print_system_info()
 casia = ImageLoaderCASIAIris('./databases/CASIA-Iris-Lamp-100')
-signaturizer = IrisSignaturizer(casia.subjects[:5], casia.images[:5])
+signaturizer = IrisSignaturizer(casia.subjects[:500], casia.images[:500])
 signaturizer.generate_signatures()
 iris_verification(signaturizer.subjects, signaturizer.signatures)
 iris_identification(signaturizer.subjects, signaturizer.normalized_irises)
