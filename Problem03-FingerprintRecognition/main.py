c2f87a064399304b60ab951d07b439ed944b9c69
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-09-19 18:21:15 -0300

    image enhancement

--- Problem03-FingerprintRecognition/main.py
+++ Problem03-FingerprintRecognition/main.py
@@ -1,3 +1,20 @@
 from rindex28_loader import Rindex28Loader
+import cv2
+
+def image_enhancement(image):
+	mean, std = cv2.meanStdDev(image)
+	i_max, j_max = image.shape
+	for i in xrange(i_max):
+		for j in xrange(j_max):
+			value = 150 + 95 * ((image[i,j] - mean)/std)
+			if value > 255:
+				value = 255
+			elif value < 0:
+				value = 0
+			image[i,j] = value
 
 rindex28 = Rindex28Loader('./databases/rindex28')
+for image in rindex28.images:
+	image = image_enhancement(image)
+	cv2.imshow('image', image)
+	cv2.waitKey(0)
