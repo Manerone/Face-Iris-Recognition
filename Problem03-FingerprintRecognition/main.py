810c86a3683d16f4659556eca69d744dfe9d0e63
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-09-26 18:59:40 -0300

    printing line

--- Problem03-FingerprintRecognition/main.py
+++ Problem03-FingerprintRecognition/main.py
@@ -37,6 +37,26 @@ def orientation_computation(image):
     return orientations
 
 
+def insert_orientation_lines:
+    o = 0
+    for j in xrange(5, 300, 10):
+        for i in xrange(5, 300, 10):
+            angle = orientations[o]
+            m = np.tan(angle)
+            # x0 e y0 são o centro do bloco
+            if m > 1:
+                # varia x + 4 e x - 4
+                # calcula y = m(x-x0) + y0
+                pass
+            else:
+                # varia y + 4 e y - 4
+                # calcula x = ((y - y0)/m) + x0
+                pass
+            f_point = (int(i + 7 * np.cos(angle)), int(j + 7 * np.sin(angle)))
+            cv2.line(image, (i, j), f_point, (0, 0, 0))
+            o += 1
+
+
 def region_of_interest_detection(image):
     for i in xrange(30):
         tmpImg = image[i * 10:(i + 1) * 10]
@@ -51,12 +71,6 @@ for image in rindex28.images:
     blurred_image = cv2.medianBlur(image_enhanced, 5)
     orientations = orientation_computation(blurred_image)
     # interesting_image = region_of_interest_detection(image_enhanced)
-    o = 0
-    for j in xrange(5, 300, 10):
-        for i in xrange(5, 300, 10):
-            angle = orientations[o]
-            f_point = (int(i + 7 * np.cos(angle)), int(j + 7 * np.sin(angle)))
-            cv2.line(image, (i, j), f_point, (0, 0, 0))
-            o += 1
+    insert_orientation_lines(image, orientations)
     plt.imshow(image, cmap='Greys_r')
     plt.show()
