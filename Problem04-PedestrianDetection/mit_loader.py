76e17b4ad5e03686fc17ddbfaf86e64679faec4c
Matheus Agio Nerone <man13@inf.ufpr.br>
2016-10-31 18:10:53 -0200

    adding mit_loader

---
+++ Problem04-PedestrianDetection/mit_loader.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+import numpy as np
+from sys import stdout
+
+
+CLEAR_LINE = '\x1b[2K'
+
+class MITLoader:
+    """docstring for MITLoader"""
+    def __init__(self, path):
+        self.path = os.path.abspath(path)
+        stdout.write('\rLoading database...')
+        stdout.flush()
+        self.get_images(self.path)
+        stdout.write('\r' + CLEAR_LINE)
+        stdout.flush()
+    
+    def get_images(self, path):
+        self.images = []
+        for image_path in self.get_files_from(path):
+            image_pil = Image.open(image_path).convert('L')
+            image_np = np.array(image_pil, 'uint8')
+            self.images.append(image_np)
+
+    def get_files_from(self, path):
+        files = []
+        for element in os.listdir(path):
+            path_to_element = os.path.join(path, element)
+            if os.path.isfile(path_to_element):
+                files.append(path_to_element)
+        return np.sort(files)
