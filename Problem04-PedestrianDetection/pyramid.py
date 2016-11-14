9af69ca7cfb496953856f90b4550544f14f275f6
MatheusAN <matheus.agio@hotmail.com>
2016-11-14 14:23:11 -0200

    Fixing identation

--- Problem04-PedestrianDetection/pyramid.py
+++ Problem04-PedestrianDetection/pyramid.py
@@ -1,15 +1,14 @@
 from skimage.transform import pyramid_gaussian
 
+
 class Pyramid:
-	"""docstring for Pyramid"""
-	@staticmethod
-	def call(image, downscale=2, minSize=(128, 64)):
-		for (i, resized) in enumerate(pyramid_gaussian(image, downscale=downscale)):
-			# if the image is too small, break from the loop
-			heigth, width, channels = resized.shape
-			if heigth < minSize[0] or width < minSize[1]:
-				break
-				
-			# show the resized image
-			yield resized
-		
+    """docstring for Pyramid"""
+    @staticmethod
+    def call(image, downscale=2, minSize=(128, 64)):
+        for (i, resized) in enumerate(pyramid_gaussian(image, downscale=downscale)):
+            # if the image is too small, break from the loop
+            heigth, width, channels = resized.shape
+            if heigth < minSize[0] or width < minSize[1]:
+                break
+            # show the resized image
+            yield resized
