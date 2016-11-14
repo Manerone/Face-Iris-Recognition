735be43f581b9ab132f483e4a9aadaef9a4c4600
MatheusAN <matheus.agio@hotmail.com>
2016-11-14 15:11:35 -0200

    windonizing orientations and magnitudes

--- Problem04-PedestrianDetection/pedestrian_detector.py
+++ Problem04-PedestrianDetection/pedestrian_detector.py
@@ -2,6 +2,9 @@ from hog import HOG
 from pyramid import Pyramid
 from windownize import Windownize
 from gradient import Gradient
+import numpy as np
+import cv2
+import math
 import matplotlib.pyplot as plt
 
 
@@ -20,9 +23,11 @@ class PedestrianDetector:
         for image in self.images:
             for img in self._pyramidize(image):
                 img_ori, img_mag = self._gradient(img)
-                for window in self._windownize(img):
-                    plt.imshow(window)
-                    plt.show()
+                a = self._windownize(img_ori)
+                b = self._windownize(img_mag)
+                for window_ori, window_mag in zip(a, b):
+                    # self._show(window_ori, window_mag)
+                    pass
 
     def _pyramidize(self, image):
         return Pyramid.call(
@@ -38,3 +43,16 @@ class PedestrianDetector:
 
     def _gradient(self, image):
         return Gradient().call(image)
+
+    def _show(self, orientations, magnitudes):
+        heigth, width = orientations.shape
+        img = np.ones((heigth, width))
+        img.fill(255)
+        for i in xrange(heigth):
+            for j in xrange(width):
+                x = (int)(j + magnitudes[i][j] * math.cos(orientations[i][j]))
+                y = (int)(i + magnitudes[i][j] * math.sin(orientations[i][j]))
+                point2 = (x, y)
+                cv2.line(img, (j, i), point2, (0, 0, 0))
+        plt.imshow(img, cmap='Greys')
+        plt.show()
