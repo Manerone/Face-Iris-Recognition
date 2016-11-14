2ee574c8f09ac90966313f5450d2fbee691364ae
MatheusAN <matheus.agio@hotmail.com>
2016-11-14 14:23:23 -0200

    Returning window

--- Problem04-PedestrianDetection/windownize.py
+++ Problem04-PedestrianDetection/windownize.py
@@ -1,10 +1,8 @@
 class Windownize:
-	@staticmethod
-	def call(image, windowSize=(128, 64), displacement=8):
-		heigth, width = windowSize
-		img_heigth, img_width, _ = image.shape
-		for i in xrange(0, img_heigth - heigth + 1, displacement):
-			for j in xrange(0, img_width - width + 1, displacement):
-				pass
-		
-		
+    @staticmethod
+    def call(image, windowSize=(128, 64), displacement=8):
+        heigth, width = windowSize
+        img_heigth, img_width, _ = image.shape
+        for i in xrange(0, img_heigth - heigth + 1, displacement):
+            for j in xrange(0, img_width - width + 1, displacement):
+                yield image[i:i + heigth, j:j + width]
