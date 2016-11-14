9c562922fdd9132cd2f997181c2db9846d98670b
MatheusAN <matheus.agio@hotmail.com>
2016-11-14 14:23:01 -0200

    Fixing identation

--- Problem04-PedestrianDetection/hog.py
+++ Problem04-PedestrianDetection/hog.py
@@ -1,15 +1,13 @@
 from adjust_gamma import AdjustGamma
 import matplotlib.pyplot as plt
 
-class HOG:
 
-	def __init__(self):
-		self.configurations = {
-			'gamma': 2.0
-		}
-	
-	def calculate(self, image):
-		# gamma_corrected = AdjustGamma.call(image, self.configurations['gamma'])
-		pass
-		
-		
+class HOG:
+    def __init__(self):
+        self.configurations = {
+            'gamma': 2.0
+        }
+
+    def calculate(self, image):
+        # gamma_corrected = AdjustGamma.call(image, self.configurations['gamma'])
+        pass
