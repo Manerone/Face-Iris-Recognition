3b550dc64fd36870738ca4d348cee46d71c24c84
Matheus Nerone <matheus@cwi.nl>
2020-11-23 10:10:40 -0300

    removing unsolved problems from main

--- Problem04-PedestrianDetection/blocknizer.py
+++
@@ -1,8 +0,0 @@
-class Blocknizer:
-    @staticmethod
-    def call(matrix, size=(16, 16), displacement=8):
-        heigth, width = matrix.shape
-        blockHeigth, blockWidth = size
-        for i in xrange(0, heigth - blockHeigth + 1, displacement):
-            for j in xrange(0, width - blockWidth + 1, displacement):
-                yield matrix[i:i + blockHeigth, j: j + blockWidth]
--- Problem04-PedestrianDetection/main.py
+++
@@ -1,7 +0,0 @@
-from mit_loader import MITLoader
-from pedestrian_detector import PedestrianDetector
-
-
-loader = MITLoader('./databases/pedestrians128x64/')
-detector = PedestrianDetector(loader.images)
-detector.train()
